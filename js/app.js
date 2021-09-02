7f47ca38a81840d95072c5d636d5114aacb199c5
Hyperactive-knucklehead <s.rahmanrahi15@gmail.com>
2021-09-02 12:53:58 +0600

    finished successfully

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
-// fetching data from api
+/* --------------------fetching data from api----------- */
 const fetchData = async (url) => {
   const res = await fetch(url);
   const data = await res.json();
   return data;
 };
 
-// getting all elements by id
+/* -----------------getting all elements by id------------ */
 const searchField = document.getElementById("search-field");
 const searchBtn = document.getElementById("search-btn");
 const booksContainer = document.getElementById("books");
 const searchResults = document.getElementById("search-results");
+const spinner = document.getElementById("spinner");
 
-//load data
+/* -------------------load data--------------------- */
 searchBtn.addEventListener("click", (e) => {
-  removeError();
   e.preventDefault();
   if (searchField.value.length > 0) {
+    booksContainer.textContent = "";
+    spinner.classList.remove("d-none");
     loadData();
   } else {
+    // giving error when search field is empty
+
     searchField.classList.add("border-3", "border-danger");
     searchResults.innerText = "please search your favorite book";
     searchResults.classList.add("fw-bold");
@@ -27,8 +31,8 @@ searchBtn.addEventListener("click", (e) => {
 
 //removing error from search field when input is given
 const removeError = () => {
+  debugger;
   searchField.classList.remove("border-3", "border-danger");
-  searchResults.innerText = "";
   searchResults.classList.remove("fw-bold");
   searchResults.removeAttribute("style", "color:#ff8c8c !important");
 };
@@ -44,9 +48,10 @@ const loadData = () => {
     .finally(() => (searchField.value = ""));
 };
 
-//display data
+/* ------------------display data---------------- */
 const displayData = (books) => {
   booksContainer.textContent = "";
+  spinner.classList.add("d-none");
   if (books.length === 0) {
     searchResults.innerText = "NO RESULTS FOUND";
   } else {
@@ -58,17 +63,17 @@ const displayData = (books) => {
         bookDiv.innerHTML = `
         <div class="card h-100">
             <img src="https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" style="width: 337px;
-            height: 345px;" class="card-img-top" alt="book" />
+            height: 345px;" class="card-img-top img-fluid" alt="book" />
                 <div class="card-body">
                   <h5 class="card-title">${title}</h5>
                   <div class="card-text">
-                    <p>author name : ${
+                    <p class="lead fw-normal" >Author name : <span class="text-primary" >${
                       book.author_name?.[0] ? book.author_name[0] : ""
-                    }<p/>
-                    <p>publisher : ${
+                    }</span><p/>
+                    <p class="lead fw-normal" >Publisher : <span class="text-primary" >${
                       book.publisher?.[0] ? book.publisher[0] : ""
-                    }<p/>
-                    <p>first published : ${
+                    }</span><p/>
+                    <p>First published : ${
                       first_publish_year ? first_publish_year : ""
                     }<p/>
                   </div>
@@ -82,7 +87,8 @@ const displayData = (books) => {
     if (booksContainer.childElementCount === 0) {
       searchResults.innerHTML = `${books.length} SEARCH RESULTS FOUND FOR <strong>"${searchField.value}"</strong> AND AMONG THEM NONE ARE SHOWN BELOW"`;
     } else {
-      searchResults.innerHTML = `${books.length} SEARCH RESULTS FOUND FOR <strong>"${searchField.value}"</strong> AND AMONG THEM ${booksContainer.childElementCount} ARE SHOWN BELOW"`;
+      searchResults.innerHTML = `${books.length} SEARCH RESULTS FOUND FOR <strong>"${searchField.value}"</strong> AND AMONG THEM ${booksContainer.childElementCount} RESULTS ARE SHOWN BELOW"`;
     }
+    removeError();
   }
 };
